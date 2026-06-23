af3d3b02503a02ea6a5476b806c555637e3ce3bb
Lisgevan <episimo@hotmail.com>
2026-06-23 19:30:35 +0300

    Section 01 completed

---
+++ Section_001/task.py
@@ -0,0 +1,36 @@
+# 6. Printing to the Console in Python
+'''
+print("Hello World!")
+'''
+
+# 7. String Manipulation and Code Intelligence
+'''
+print("Hello World!\nHello World!\nHello World!")
+print("Hello" + " " + "Vangelis")
+'''
+
+# 8. The Python Input Function
+'''
+input("What is your name?")
+print("hello " + input("What is your name?"))
+print("hello " + input("What is your name?") + "!")
+'''
+
+# 9. Python Variables
+'''
+name = input("What is your name?")
+print(len(name))
+---------------------------------
+username = input("What is your name?")
+length = len(username)
+print(length)
+'''
+
+# 11. Day 1 Project: Band Name Generator
+'''
+'''
+print("Welcome to the Band Name Generator.\n")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
+
